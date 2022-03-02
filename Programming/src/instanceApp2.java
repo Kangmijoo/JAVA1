2316b67f3c126287e72b24489ebf7f18e06b489c
Kang mijoo <kangmj12310@gmail.com>
2022-03-03 01:00:26 +0900

    Add a description

--- Programming/src/instanceApp2.java
+++ Programming/src/instanceApp2.java
@@ -7,10 +7,15 @@ public class instanceApp2 {
 
 	public static void main(String[] args) throws IOException {
 		
-		// PrintWriter클래스 사용
+		// PrintWriter클래스 사용 
+		// 왼쪽 클릭 -> Open Type Hierarchy에서 부모 클래스 확인 가능
+		// Object > Writer > PrintWriter
 		PrintWriter p2 = new PrintWriter("result2.txt");
 		p2.write("hello2");
 		p2.close();
+		
+		// PrintWriter의 부모 클래스인 Object에 있는 메소드를(toString..)를 상속받았기 때문에 사용할 수 있음
+		p2.toString();	
 
 	}
 
