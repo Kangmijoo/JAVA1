9bfcd681da7bfc27238aaf3c3b48197f5d7ba70a
Kang mijoo <kangmj12310@gmail.com>
2022-03-22 22:09:08 +0900

    정수 그대로 출력하기

---
+++ CodeUp/src5/C1110.java
@@ -0,0 +1,15 @@
+/* 입력받은 정수를 그대로 출력한다. 
+ * 입력 : 5	-> 출력 : 5	*/
+import java.util.Scanner;
+
+public class C1110 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.close();
+		
+		System.out.println(num);
+	}
+
+}
