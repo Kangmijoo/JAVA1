abba32344422bebfd6cc64e0c496505de723fd5a
Kang mijoo <kangmj12310@gmail.com>
2022-03-22 18:07:00 +0900

    반복문 출력하기1

---
+++ CodeUp/src5/C1107.java
@@ -0,0 +1,17 @@
+/* print 단어를 연속으로 50번 출력하시오.(띄어쓰기 없음) printprintprint...print (50번 출력)	*/
+public class C1107 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 50; i++) {
+			System.out.print("print");
+		}
+		
+//		int n = 0;
+//		while(n < 50) {
+//			System.out.print("print");
+//			n++;
+//		}
+
+	}
+
+}
