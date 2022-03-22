bc3e2ccc81a93216e8757416e65c8eea9d3e414f
Kang mijoo <kangmj12310@gmail.com>
2022-03-23 01:51:17 +0900

    두 정수 순서 바꿔서 출력

--- CodeUp/src5/C1113.java
+++ CodeUp/src5/C1113.java
@@ -1,10 +1,16 @@
+/* 두 정수를 입력받아 순서를 바꿔서 출력하시오.
+ * 입력 : 1 2	 -> 출력 : 2 1	*/
 import java.util.Scanner;
 
 public class C1113 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		sc.close();
+		
+		System.out.println(num2 +" "+ num1);
 	}
 
 }
