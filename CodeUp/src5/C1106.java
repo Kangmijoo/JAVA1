593ba36abbf3f53e9a4afe1b1ec7c0c06a1723ab
Kang mijoo <kangmj12310@gmail.com>
2022-03-22 18:01:53 +0900

    Int의 범위

--- CodeUp/src5/C1106.java
+++ CodeUp/src5/C1106.java
@@ -1,4 +1,5 @@
-/* int형의 가장 작은수a와 int형의 가장 큰수b를 차례대로 공백으로 분리하여 출력한다. */
+/* int형의 가장 작은수a와 int형의 가장 큰수b를 차례대로 공백으로 분리하여 출력한다. 
+ * 출력 : -2147483648 2147483647	*/
 public class C1106 {
 
 	public static void main(String[] args) {
