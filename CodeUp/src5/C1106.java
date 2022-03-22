2b110962f63552613063cf40866c39cf2a78eb6f
Kang mijoo <kangmj12310@gmail.com>
2022-03-22 18:01:23 +0900

    Int의 범위

---
+++ CodeUp/src5/C1106.java
@@ -0,0 +1,10 @@
+/* int형의 가장 작은수a와 int형의 가장 큰수b를 차례대로 공백으로 분리하여 출력한다. */
+public class C1106 {
+
+	public static void main(String[] args) {
+		int a = Integer.MIN_VALUE;
+		int b = Integer.MAX_VALUE;
+		System.out.print(a +" "+ b);
+	}
+
+}
