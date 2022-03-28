161653db05411c7e163889c3877d1b320c35615d
Kang mijoo <kangmj12310@gmail.com>
2022-03-28 16:27:50 +0900

    두 수 비교하기

--- Baekjoon/조건문/C1.java
+++ Baekjoon/조건문/C1.java
@@ -1,9 +1,23 @@
+/* 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오. (-10,000 ≤ A, B ≤ 10,000)
+ * A가 B보다 큰 경우에는 '>'를 출력한다. A가 B보다 작은 경우에는 '<'를 출력한다. A와 B가 같은 경우에는 '=='를 출력한다.
+ * 입력 : 1 2	-> 출력 : <	*/
+import java.util.Scanner;
 
 public class C1 {
 
 	public static void main(String[] args) {
-		System.out.println("he");
-
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		if(a < b) {
+			System.out.println("<");
+		} else if(a > b) {
+			System.out.println(">");
+		} else {
+			System.out.println("==");
+		}
 	}
 
 }
