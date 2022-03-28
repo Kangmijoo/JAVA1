5d08df487ee480d5a1e17d3e2928a2664a944b10
Kang mijoo <kangmj12310@gmail.com>
2022-03-28 16:12:07 +0900

    *미완

---
+++ CodeUp/src6/C1168.java
@@ -0,0 +1,13 @@
+/* 생년월일(6자리)과 성별정보(1자리)가 공백으로 분리되어 정수로 주어진다. 2012년도에 현재 나이를 출력하시오.
+ * 입력 : 790101 1	-> 출력 : 34	*/
+import java.util.Scanner;
+
+public class C1168 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
