0c2c68a9ecd9f30b11f10154a58b1f344d95ca2a
Kang mijoo <kangmj12310@gmail.com>
2022-03-28 18:19:01 +0900

    1부터 N까지의 합을 구하는 문제

--- Baekjoon/반복문/C03.java
+++ Baekjoon/반복문/C03.java
@@ -1,10 +1,19 @@
+/* n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오. 첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+ * 입력 : 3	-> 출력 : 6	*/
 import java.util.Scanner;
 
 public class C03 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		int n = sc.nextInt();
+		sc.close();
+		
+		long result = 0;
+		for(int i = 1; i <= n; i++) {
+			result += i;
+		}
+		System.out.println(result);
 	}
 
 }
