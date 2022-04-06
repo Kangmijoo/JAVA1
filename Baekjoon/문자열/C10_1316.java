3a7ccbb32fea609fb84aae5fc0ffcb114e0bed38
Kang mijoo <kangmj12310@gmail.com>
2022-04-06 20:34:30 +0900

    미완

---
+++ Baekjoon/문자열/C10_1316.java
@@ -0,0 +1,29 @@
+/* 그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다. 예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고,
+ * kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만, aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
+ * 단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
+ * 입력 : 3 \ happy \ new \ year	-> 출력 : 3	/ 입력 : 4 \ aba \ abab \ abcabc \ a	-> 출력 : 1	
+ * 입력 : 5 \ ab \ aa \ aca \ ba \ bb	 -> 출력 : 4	/ 입력 : 2 \ yzyzy \ zyzyz	-> 출력 : 0	*/
+import java.util.Scanner;
+
+public class C10_1316 {
+
+	public static boolean check() {
+		boolean[] check = new boolean[26];
+		
+		return ;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = 0; i < N; i++) {
+			String str = sc.nextLine();
+			
+			
+			}
+			
+		sc.close();
+		
+	}
+
+}
