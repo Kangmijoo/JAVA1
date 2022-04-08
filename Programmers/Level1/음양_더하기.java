bda6200ed131932a726969afc45b5c3001685ef7
Kang mijoo <kangmj12310@gmail.com>
2022-04-08 15:55:25 +0900

    두개의 배열을 가져와서 음양 더하기

---
+++ Programmers/Level1/음양_더하기.java
@@ -0,0 +1,26 @@
+/* 어떤 정수들이 있습니다. 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다. 
+ * 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요. (true면 +, false면 -)
+ * 입력 : [4,7,12] [true,false,true]	-> 출력 : 9	/ 입력 : [1,2,3] [false,false,true]	-> 출력 : 0	*/
+public class 음양_더하기 {
+
+	class Solution {
+	    public int solution(int[] absolutes, boolean[] signs) {
+	        int answer = 0;
+	        
+	        for(int i = 0; i < absolutes.length; i++) {
+	            if(signs[i] == true) {
+	                answer += absolutes[i];
+	            } else {
+	                answer -= absolutes[i];
+	            }
+	        }
+	        return answer;
+	    }
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
