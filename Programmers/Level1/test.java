fda24db39ede117987a5c8e3cf14400f1b69e679
Kang mijoo <kangmj12310@gmail.com>
2022-04-30 23:55:32 +0900

    -

--- Programmers/Level1/test.java
+++ Programmers/Level1/test.java
@@ -1,29 +1,18 @@
-import java.util.Scanner;
+
+import java.util.*;
 
 public class test {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		sc.close();
-		String s = "a B z";
-		int n = 4;
-		String answer = "";
+		int answer = 0;
+		int[] lost = {2,4};
+		int[] reserve = {1,3,5};
 		
-        int b = 0;
-        for(int i = 0; i < s.length(); i++) {
-        	if(s.charAt(i) == ' ') {
-        		b = 32;
-        	} else {
-        		b = (int)s.charAt(i)+n;
-        	}
-            if(b > 122 || (s.charAt(i) < 91 && b > 90)) {
-            	b = b-26;
-            }
-            answer += (char)b;
-        }
+		for(int i = 0; i < lost.length; i++)
 		
 		System.out.println(answer);
-		
 	}
 
 }
