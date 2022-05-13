038eecb91cc41bf4c1e8d5e66f58096d63c755ed
Kang mijoo <kangmj12310@gmail.com>
2022-05-13 20:17:33 +0900

    @

---
+++ Baekjoon/정렬/C08_1181.java
@@ -0,0 +1,26 @@
+/* 알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오. 길이가 짧은 것부터, 길이가 같으면 사전 순으로 
+ * 입력 : 13 \ but \ i \ wont \ hesitate \ no \ more \ no \ more \ it \ cannot \ wait \ im \ yours
+ * 출력 : i \ im \ it \ no \ but \ more \ wait \ wont \ yours \ cannot \ hesitate		*/
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class C08_1181 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		ArrayList<String> list = new ArrayList<>();		
+		String s = "";
+		for(int i = 0; i < N; i++) {
+			s = sc.nextLine();
+			list.add(s.length()+s);
+		}
+		sc.close();
+		
+		Collections.sort(list.subList(0, 0));
+		System.out.println(list);
+	}
+
+}
