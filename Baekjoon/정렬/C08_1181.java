451243eb7a2570650f411e74c6e087bba0c5b97f
Kang mijoo <kangmj12310@gmail.com>
2022-05-25 22:53:22 +0900

    ***단어 길이 순으로 정렬

--- Baekjoon/정렬/C08_1181.java
+++ Baekjoon/정렬/C08_1181.java
@@ -8,16 +8,32 @@ public class C08_1181 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		ArrayList<String> list = new ArrayList<>();		
-		String s = "";
+		String[] arr = new String[N];
+		sc.nextLine();	//개행 버림
+		
 		for(int i = 0; i < N; i++) {
-			s = sc.nextLine();
-			list.add(s.length()+s);
+			arr[i] = sc.nextLine();
 		}
 		sc.close();
 		
+		Arrays.sort(arr, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				if(s1.length() == s2.length()) {	//단어 길이 같을 때
+					return s1.compareTo(s2);
+				} else {
+					return s1.length() - s2.length();
+				}
+			}
+		});		
 		
-		System.out.println(list);
+		System.out.println(arr[0]);
+		
+		for(int i = 1; i < N; i++) {
+			//중복되지 않는 단어만 출력
+			if(!arr[i].equals(arr[i - 1])) {
+				System.out.println(arr[i]);
+			}
+		}
 	}
 
 }
