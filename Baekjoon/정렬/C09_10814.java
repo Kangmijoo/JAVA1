9ad2edd72d882183c6b8b82359fddf7405c4093c
Kang mijoo <kangmj12310@gmail.com>
2022-05-26 23:44:52 +0900

    ***나이순 정렬(나이 정렬, 나이 같으면 이름 순으로 정렬)

--- Baekjoon/정렬/C09_10814.java
+++ Baekjoon/정렬/C09_10814.java
@@ -1,26 +1,28 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class C09_10814 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int[] age = new int[N];
-		String[] name = new String[N];
-		String[] info = new String[N];
-		for(int i = 0; i < N; i++) {
-			age[i] = sc.nextInt();
-			name[i] = sc.nextLine();
-			info[i] = age[i] +" "+ name[i];
-		}
-		sc.close();
+		String[][] arr = new String[N][2];
 		
-		Arrays.sort(info);
-
 		for(int i = 0; i < N; i++) {
-			System.out.println(info[i]);
+			arr[i][0] = sc.next();
+			arr[i][1] = sc.next();
 		}
+		
+		Arrays.sort(arr, new Comparator<String[]>() {
+			@Override
+			public int compare(String[] s1, String[] s2) {
+				return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+			}
+		});
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i][0] +" "+ arr[i][1]);
+		}
+		
 	}
 
 }
